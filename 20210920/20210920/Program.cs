df06b8d5cbde4e92649a95e77bcd556facfeeb79
ZsobrakMark <90597678+ZsobrakMark@users.noreply.github.com>
2021-09-20 09:31:53 +0200

    8-ig

--- 20210920/20210920/Program.cs
+++ 20210920/20210920/Program.cs
@@ -15,6 +15,11 @@ namespace _20210920
             //feladat3();
             //feladat4();
             //feladat5();
+            //feladat6();
+            //feladat7();
+            feladat8();
+
+
         }
         static void feladat1()
         {
@@ -34,7 +39,7 @@ namespace _20210920
         {
             Console.WriteLine("Kérek egy számot :");
             int szam = Convert.ToInt32(Console.ReadLine());
-            if (szam<0)
+            if (szam < 0)
             {
                 Console.WriteLine("A szám negatív");
             }
@@ -48,7 +53,7 @@ namespace _20210920
         {
             Console.WriteLine("Hány fok van kint? :");
             int homerseklet = Convert.ToInt32(Console.ReadLine());
-            if (homerseklet<0)
+            if (homerseklet < 0)
             {
                 Console.WriteLine("Fagy van kint");
             }
@@ -62,7 +67,7 @@ namespace _20210920
         {
             Console.WriteLine("Kérek egy számot");
             int szam = Convert.ToInt32(Console.ReadLine());
-            if (-30<szam && szam<40)
+            if (-30 < szam && szam < 40)
             {
                 Console.WriteLine("A szám a tartományban van");
             }
@@ -76,17 +81,77 @@ namespace _20210920
         {
             Console.WriteLine("Kérek egy számot :");
             int szam = Convert.ToInt32(Console.ReadLine());
-            if (szam<16)
+            if (szam < 16)
             {
                 szam = szam * 16;
                 Console.WriteLine(szam);
             }
-            else if(szam>=16)
+            else if (szam >= 16)
             {
                 szam = szam / 3;
                 Console.WriteLine(szam);
             }
             Console.ReadLine();
         }
+        static void feladat6()
+        {
+            Console.WriteLine("Kérek egy számot 10 és 50 között :");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (10 <= szam && szam <= 19)
+            {
+                Console.WriteLine("Tizesek közé tartozik");
+            }
+            else if (20 <= szam && szam <= 29)
+            {
+                Console.WriteLine("huszasok közé tartozik");
+            }
+            else if (30 <= szam && szam <= 39)
+            {
+                Console.WriteLine("Harmincasok közé tartozik");
+            }
+            else if (40 <= szam && szam <= 49)
+            {
+                Console.WriteLine("negyvenesek közé tartozik");
+            }
+            Console.ReadLine();
+        }
+        static void feladat7()
+        {
+            Console.WriteLine("Kérek egy számot : ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (12 < szam && szam < 25 && szam % 2 == 0)
+            {
+                Console.WriteLine("A tartományban lévő szám páros");
+            }
+            else
+            {
+                Console.WriteLine("A szám nincs a tartományban vagy nem páros");
+            }
+            Console.ReadLine();
+        }
+        static void feladat8()
+        {
+            Console.WriteLine("Kérem a víz hőmérsékletét");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if(szam<0)
+            {
+                Console.WriteLine("A víz szilárd halmazállapotú");
+            }
+            else if(0<=szam && szam<100)
+            {
+                Console.WriteLine("A víz folyékony halmazállapotú");
+            }
+            else if(100<=szam)
+            {
+                Console.WriteLine("A víz gáz halmazállapotú");
+            }
+            Console.ReadLine();
+        }
+        
+        
+        
+
+        
+
     }
 }
